3fb31f0efc7f2fffad0d922e2326e9ec0b097a6d
hshockney <shockney@atlanticbb.net>
2019-08-02 14:28:28 -0400

    Removed unneeded files

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_02_161437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "examples", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
